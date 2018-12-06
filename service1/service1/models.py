aeb5e9181833eb63eec9c29f26fee8eefc572e63
Stepan Bakshayev <step2back@gmail.com>
2018-12-06 16:19:33 +0700

    [2 h 30 min] designed the schema

---
+++ service1/service1/models.py
@@ -0,0 +1,27 @@
+from enum import Enum
+from operator import attrgetter
+
+from django.core import validators
+from django.db import models
+
+
+class Function(models.Model):
+    class Type(Enum):
+        first = 'y=t*t+2/t'
+        second = 'y=sin(t)'
+
+    type = models.CharField(choices=tuple((t.value, t.name) for t in Type), max_length=max(map(len, map(attrgetter('value'), Type))))
+    interval = models.IntegerField(validators=(validators.MinValueValidator(1),))
+    step = models.IntegerField(validators=(validators.MinValueValidator(1),))
+
+
+class Chart(models.Model):
+    function = models.ForeignKey(Function, unique=True, on_delete=models.CASCADE)
+    chart = models.ImageField(upload_to='chart/')
+    generated_at = models.DateTimeField()
+
+
+class Error(models.Model):
+    function = models.ForeignKey(Function, unique=True, on_delete=models.CASCADE)
+    message = models.TextField()
+    generated_at = models.DateTimeField()
